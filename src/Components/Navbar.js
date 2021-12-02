4db219125bf87715f116e7cb4f61cb9a9c2e4a90
Chavan Saurabh Ravindra <saurabhchavan1223@gmail.com>
2021-12-02 18:20:39 +0530

    update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,72 +1,91 @@
 
 import React from 'react';
-import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
 const Navbars = () => {
     return (
         <>
-            <Navbar expand="lg" stiky="top" className="fs-6 fw-lighter navbar" >
-                <Container className="nav">
-
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto text-white">
-                            <Nav.Link href="/" className="px-1 text-white fw-bolder">Home</Nav.Link>
-
-                            <NavDropdown title="About" id="basic-nav-dropdown" className="px-1">
-                                <NavDropdown.Item href="#action/3.1">About Institute</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Mission, Vision,Goals {`&`} Core Values</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Right To Information Act</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Contact us</NavDropdown.Item>
-                            </NavDropdown>
 
 
-                            <NavDropdown title="Departments" id="basic-nav-dropdown" className="px-1">
-                                <NavDropdown.Item href="#action/3.1">Civil Engineering</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Mechanical Engineering</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Electronic {`&`} Telecommunication Engineering</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Automobile Engineering</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.5">Instrumentation {`&`} Control Engineering</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.6">Admission section</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.7">Applied Science</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.8">Office</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.9">Workshop</NavDropdown.Item>
-                            </NavDropdown>
-
-                            <NavDropdown title="Facilities" id="basic-nav-dropdown" className="px-1">
-                                <NavDropdown.Item href="#action/3.1">Boys Hostel</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Girls Hostel</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Gymkhana</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Library</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">EDSUC</NavDropdown.Item>
-                            </NavDropdown>
-
-                            <NavDropdown title="Admission" id="basic-nav-dropdown" className="px-1">
-                                <NavDropdown.Item href="#action/3.1">FY Admission</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">DSY admission</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">FEE STRUCTURE</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Admitted Candidate 2020-21</NavDropdown.Item>
-
-                            </NavDropdown>
-
-                            <Nav.Link href="/notices" className="px-1">Notices</Nav.Link>
-
-                            <NavDropdown title="Placements" id="basic-nav-dropdown" className="px-1">
-                                <NavDropdown.Item href="#action/3.1">TPO's Message</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">TPO Activities</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Collaborations</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">TPO Events</NavDropdown.Item>
-                            </NavDropdown>
-
-                            <Nav.Link href="/tenders" className="px-1">Tenders</Nav.Link>
-
-                            <Nav.Link href="/gallery" className="px-4">Gallery</Nav.Link>
-
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+        <header className="header">
+            <div className="container">
+                <div className="header-main">
+                    <button type="button" className="header-hamburger-btn js-header-menu-toggler">
+                        <span> </span>
+                    </button>
+                    <div className="header-backdrop js-header-backdrop"></div>
+                    <nav className="header-menu js-header-menu">
+                        <button type="button" className="header-close-btn js-header-menu-toggler">
+                            <i class="fas fa-times"></i>
+                        </button>
+                        <ul className="menu">
+                            <li className="menu-item"><a href="index.html">Home</a></li>
+                            <li className="menu-item menu-item-has-children">
+                                <a href="#" className="js-toggle-sub-menu">About<i className="fas fa-chevron-down"></i></a>
+                                <ul className="sub-menu js-sub-menu">
+                                    <li className="sub-menu-item"><a href="">Mission, Vision,Goals {`&`} Core Values</a></li>
+                                    <li className="sub-menu-item"><a href="">Right To Information Act</a></li>
+                                    <li className="sub-menu-item"><a href="">Contact us</a></li>
+                                </ul>
+                            </li>
+                            <li className="menu-item menu-item-has-children">
+                                <a href="#" className="js-toggle-sub-menu">Departments<i className="fas fa-chevron-down"></i></a>
+                                <ul className="sub-menu js-sub-menu">
+                                    <li className="sub-menu-item"><a href="">Civil Engineering</a></li>
+                                    <li className="sub-menu-item"><a href="">Mechanical Engineering</a></li>
+                                    <li className="sub-menu-item"><a href="">Computer Engineering</a></li>
+                                    <li className="sub-menu-item"><a href="">Electronic {`&`} Telecommunication Engineering</a></li>
+                                    <li className="sub-menu-item"><a href="">Automobile Engineering</a></li>
+                                    <li className="sub-menu-item"><a href="">Instrumentation {`&`} Control Engineering</a></li>
+                                    <hr></hr>
+                                    <li className="sub-menu-item"><a href="">Admission section</a></li>
+                                <li className="sub-menu-item"><a href="">Applied Science</a></li>
+                                <li className="sub-menu-item"><a href="">Office</a></li>
+                                <li className="sub-menu-item"><a href="">Workshop</a></li>
+                                </ul> 
+                            </li>
+                            <li class="menu-item menu-item-has-children">
+                                <a href="#" class="js-toggle-sub-menu"> Facilities <i className="fas fa-chevron-down"></i></a>
+                                <ul class="sub-menu js-sub-menu">
+                                    <li className="sub-menu-item"><a href=""> Boys Hostel </a></li>
+                                    <li className="sub-menu-item"><a href=""> Girls Hostel</a></li>
+                                    <li className="sub-menu-item"><a href=""> Gymkhana</a></li>
+                                    <li className="sub-menu-item"><a href=""> Library</a></li>
+                                    <li className="sub-menu-item"><a href=""> EDSUC</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item menu-item-has-children">
+                                <a href="#" class="js-toggle-sub-menu"> Admission <i className="fas fa-chevron-down"></i></a>
+                                <ul class="sub-menu js-sub-menu">
+                                   <li className="sub-menu-item"><a href="">FY Admission</a></li>
+                                   <li className="sub-menu-item"><a href="">DSY admission</a></li>
+                                   <li className="sub-menu-item"><a href="">FEE STRUCTURE</a></li>
+                                   <li className="sub-menu-item"><a href="">Admitted Candidate 2020-21</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item menu-item-has-children">
+                                <a href="#" class="js-toggle-sub-menu"> Placement Cell<i className="fas fa-chevron-down"></i></a>
+                                <ul class="sub-menu js-sub-menu">
+                                    <li className="sub-menu-item"><a href="">TPO's Message</a></li>
+                                    <li className="sub-menu-item"><a href="">TPO Activities</a></li>
+                                    <li className="sub-menu-item"><a href="">Collaborations</a></li>
+                                    <li className="sub-menu-item"><a href="">TPO Events</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item menu-item-has-children">
+                                <a href="#" class="js-toggle-sub-menu"> Governance <i className="fas fa-chevron-down"></i></a>
+                                <ul class="sub-menu js-sub-menu">
+                                    <li className="sub-menu-item"><a href="">G.Redressal Committes</a></li>
+                                    <li className="sub-menu-item"><a href="">G.Redressal System</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item"><a href="">Notices</a></li>
+                            <li class="menu-item"><a href="">Tendars</a></li>
+                            <li class="menu-item"><a href="">Approval</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
         </>
     )
 }
